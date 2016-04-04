ae0aad82d8c528789a3a59c9799be89b7ec1aeef
PlawanTH <thanawat.cskmutt@mail.kmutt.ac.th>
2016-04-04 13:22:01 +0700

    Upload Project

---
+++ src/csc319_carreservation/CSC319_CarReservation.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc319_carreservation;
+
+/**
+ *
+ * @author T.Chan
+ */
+public class CSC319_CarReservation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Login();
+    }
+    
+}
