63e395e27cff57b46c9a658e0eb013cb75ef6ad9
jane5509 <chalinyasutrat@10.17.248.223>
2016-04-07 16:51:15 +0700

    Test commit

--- src/csc319_carreservation/CSC319_CarReservation.java
+++ src/csc319_carreservation/CSC319_CarReservation.java
@@ -15,7 +15,7 @@ public class CSC319_CarReservation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create log in window
         new Login();
     }
     
