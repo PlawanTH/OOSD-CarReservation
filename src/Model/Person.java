8e6fb0710751e8abfe84720ad4a84a6a870991ee
jane5509 <chalinyasutrat@192.168.1.49>
2016-05-20 17:07:13 +0700

    create person

---
+++ src/Model/Person.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author chalinyasutrat
+ */
+class Person {
+    
+    private String citizenID;
+    private String firstname;
+    private String lastname;
+    private String dateOfBirth;
+    private String telephone;
+    private String email;
+    private String address;
+    
+}
